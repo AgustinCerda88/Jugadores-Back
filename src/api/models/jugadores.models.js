1d37676766f7b5cc265a9910312cbcaf2839beb7
Agustin Caldera <agus@Agustin-2.local>
2023-04-26 14:16:11 +0200

    si

---
+++ src/api/models/jugadores.models.js
@@ -0,0 +1,17 @@
+const mongoose = require ('mongoose');
+
+const Schema = mongoose.Schema;
+
+const jugadoresSchema = new Schema (
+    {
+        "nombre": {type: String, require: true},
+        "edad": {type: String, require: true},
+        "posicion": {type: String, require: true},
+        "pierna": {type: String, require: true},
+        "foto": {type: String, require: true, default: "https://img.youtube.com/vi/3sBd4vBNnoI/hqdefault.jpg"}
+    }
+);
+
+const jugadores = mongoose.model('jugadores', jugadoresSchema);
+
+module.exports = jugadores;
